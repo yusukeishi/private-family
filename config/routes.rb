09cdbe8591a5f256feb39b006b5baebd719b12bb
yusukeishi <ishisida777@icloud.com>
2021-10-13 16:35:48 +0900

    トップページ　userモデル

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'photos/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "photos#index"
+
 end
